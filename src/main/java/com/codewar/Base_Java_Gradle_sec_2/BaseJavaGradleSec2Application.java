a20d7ad385647a7d3e6c883e1e3c146b2f012633
Victor J Hernandez <viktorjhm@gmail.com>
2022-12-28 16:34:20 -0400

    Initial commit

---
+++ src/main/java/com/codewar/Base_Java_Gradle_sec_2/BaseJavaGradleSec2Application.java
@@ -0,0 +1,13 @@
+package com.codewar.Base_Java_Gradle_sec_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseJavaGradleSec2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseJavaGradleSec2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewar/Base_Java_Gradle_sec_2/BaseJavaGradleSec2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewar.Base_Java_Gradle_sec_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseJavaGradleSec2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
