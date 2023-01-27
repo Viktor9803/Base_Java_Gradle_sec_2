a64c7db7251b13d26e819fa0e87a62ab689b8b40
Victor J Hernandez <viktorjhm@gmail.com>
2023-01-27 11:59:07 -0400

    add views & fix flyway migrations

--- src/main/resources/db/migration/V004__insert_users_role.sql
+++ src/main/resources/db/migration/V004__insert_users_role.sql
@@ -1,3 +1,3 @@
-INSERT INTO `usuarios` (username, password, active, role) VALUES ('USER', '$2a$10$1XRoHXl1X3Z42M0fOt/gFufPIDQAcPjBEiocGv1ZjFRHHK8/D0k.i', '1', 'ROLE_USER');
-INSERT INTO `usuarios` (username, password, active, role) VALUES ('ADMIN', '$2a$10$aFwv23A0wflvlL4.uH9pHeJee.iR2q5.thYbUsLINeQObGSC60WpS', '1', 'ROLE_ADMIN');
+INSERT INTO `usuarios` (username, password, fname, lname, email, age, active, role) VALUES ('USER', '$2a$10$1XRoHXl1X3Z42M0fOt/gFufPIDQAcPjBEiocGv1ZjFRHHK8/D0k.i', 'User', 'User', 'user@user.com', '25', '1', 'ROLE_USER');
+INSERT INTO `usuarios` (username, password, fname, lname, email, age, active, role) VALUES ('ADMIN', '$2a$10$aFwv23A0wflvlL4.uH9pHeJee.iR2q5.thYbUsLINeQObGSC60WpS', 'Admin', 'Admin', 'admin@admin.com', '25', '1', 'ROLE_ADMIN');
 
