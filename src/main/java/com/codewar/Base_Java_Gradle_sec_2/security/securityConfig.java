07f1190fac2eaaef5f4c6e01958ca1f82fa17873
Victor J Hernandez <viktorjhm@gmail.com>
2023-02-01 12:25:17 -0400

    Update securityConfig.java

--- src/main/java/com/codewar/Base_Java_Gradle_sec_2/security/securityConfig.java
+++ src/main/java/com/codewar/Base_Java_Gradle_sec_2/security/securityConfig.java
@@ -1,46 +1,44 @@
-package com.codewar.Base_Java_Gradle_sec_2.security;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-@Configuration
-public class securityConfig extends WebSecurityConfigurerAdapter{
-	
-	@Autowired
-	private UserDetailsService userDetailsService;
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		auth.userDetailsService(userDetailsService);
-
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		http.authorizeRequests().antMatchers("/data_java/home", "/resources/**").permitAll()
-				.antMatchers("/data/list").hasAnyRole("USER", "ADMIN")
-				.antMatchers("/data/list/data").hasAnyRole("USER", "ADMIN")
-//				.antMatchers(HttpMethod.POST, "/data/list/create/**").hasRole("ADMIN")
-//				.antMatchers("/data/list/datal/**").hasRole("ADMIN")
-//				.antMatchers(HttpMethod.POST, "/data_java/list/dataDelete/**").hasRole("ADMIN")
-//				.antMatchers(HttpMethod.POST, "/data_java/list/dataDeleteAll").hasRole("ADMIN")
-//				.antMatchers(HttpMethod.POST, "/data_java/list/update/**").hasRole("ADMIN")
-				.and()
-					.formLogin()
-						.loginPage("");
-	}
-
-	@Bean
-	public BCryptPasswordEncoder passEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
-}
+//package com.codewar.Base_Java_Gradle_sec_2.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//@Configuration
+//public class securityConfig extends WebSecurityConfigurerAdapter{
+//	
+////	@Autowired
+////	private UserDetailsService userDetailsService;
+//
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//
+//		auth.userDetailsService(null);
+//
+//	}
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//
+//		http.authorizeRequests().antMatchers("/data_java/home", "/resources/**").permitAll()
+//				.antMatchers("/data/list").hasAnyRole("USER", "ADMIN")
+//				.antMatchers("/data/list/data").hasAnyRole("USER", "ADMIN")
+////				.antMatchers(HttpMethod.POST, "/data/list/create/**").hasRole("ADMIN")
+////				.antMatchers("/data/list/datal/**").hasRole("ADMIN")
+////				.antMatchers(HttpMethod.POST, "/data_java/list/dataDelete/**").hasRole("ADMIN")
+////				.antMatchers(HttpMethod.POST, "/data_java/list/dataDeleteAll").hasRole("ADMIN")
+////				.antMatchers(HttpMethod.POST, "/data_java/list/update/**").hasRole("ADMIN")
+//				.and()
+//					.formLogin()
+//						.loginPage("");
+//	}
+//
+//	@Bean
+//	public BCryptPasswordEncoder passEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+//
+//}
