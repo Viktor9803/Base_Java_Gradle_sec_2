ba96bc534ba14f689d60ecd1acface9482381fca
Victor J Hernandez <viktorjhm@gmail.com>
2023-02-23 10:34:56 -0400

    Update securityConfig.java

--- src/main/java/com/codewar/Base_Java_Gradle_sec_2/security/securityConfig.java
+++ src/main/java/com/codewar/Base_Java_Gradle_sec_2/security/securityConfig.java
@@ -3,6 +3,7 @@ package com.codewar.Base_Java_Gradle_sec_2.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,11 +30,11 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/data/home", "/resources/**").permitAll()
 				.antMatchers("/data/list").hasAnyRole("USER", "ADMIN")
 				.antMatchers("/data/list/findAll").hasAnyRole("USER", "ADMIN")
-//				.antMatchers(HttpMethod.POST, "/data/list/create/**").hasRole("ADMIN")
-//				.antMatchers("/data/list/datal/**").hasRole("ADMIN")
-//				.antMatchers(HttpMethod.POST, "/data_java/list/dataDelete/**").hasRole("ADMIN")
-//				.antMatchers(HttpMethod.POST, "/data_java/list/dataDeleteAll").hasRole("ADMIN")
-//				.antMatchers(HttpMethod.POST, "/data_java/list/update/**").hasRole("ADMIN")
+				.antMatchers(HttpMethod.POST, "/data/list/create/**").hasRole("ADMIN")
+				.antMatchers("/data/list/findCi/**").hasRole("ADMIN")
+				.antMatchers(HttpMethod.POST, "/data/list/delete/**").hasRole("ADMIN")
+				.antMatchers(HttpMethod.POST, "/data/list/deleteAll").hasRole("ADMIN")
+				.antMatchers(HttpMethod.POST, "/data/list/update/**").hasRole("ADMIN")
 				.and()
 					.formLogin().defaultSuccessUrl("/data/home")
 					.failureUrl("/login?error").permitAll()
