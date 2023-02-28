39c4edc71d4612a9ba23fc874aa38f853140e97a
Victor J Hernandez <viktorjhm@gmail.com>
2023-02-28 11:11:37 -0400

    Update UserController.java

--- src/main/java/com/codewar/Base_Java_Gradle_sec_2/controller/UserController.java
+++ src/main/java/com/codewar/Base_Java_Gradle_sec_2/controller/UserController.java
@@ -2,16 +2,19 @@ package com.codewar.Base_Java_Gradle_sec_2.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/data")
 public class UserController {
 
-	@GetMapping(value = "/login")
+	@GetMapping(value = "/data/login")
 	public String login() {
+		return "baseJava/login";
 
-			return "baseJava/login";
+	}
+
+	@GetMapping(value = "/")
+	public String index() {
+		return "baseJava/home";
 
 	}
 
